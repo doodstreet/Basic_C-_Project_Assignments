98d1c66974696e438922985b3f9609998d3cfd88
Dstreet <doodstreet@gmail.com>
2020-11-12 15:52:19 -0800

    created Math and operators program
    changed repo name also

--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -10,18 +10,18 @@ namespace VariablesAndDataTypes
     {
         static void Main(string[] args)
         {
-            // this is a comment about a simple program
-            //Console.WriteLine("What is your name?");
-            //string yourName = Console.ReadLine();
-            //Console.WriteLine("Your name is: " + yourName);
-            //Console.ReadLine();
+            //this is a comment about a simple program
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
+            Console.ReadLine();
 
-            //Console.WriteLine("What is your favorite number?");
-            //string favoriteNumber = Console.ReadLine();
-            //int favNum = Convert.ToInt32(favoriteNumber);
-            //int total = favNum + 5;
-            //Console.WriteLine("Your favorite number plus 5 is: " + total);
-            //Console.ReadLine();
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum + 5;
+            Console.WriteLine("Your favorite number plus 5 is: " + total);
+            Console.ReadLine();
 
             bool isStudying = false;
             byte hoursWorked = 42;
