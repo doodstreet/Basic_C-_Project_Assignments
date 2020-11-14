1c4f85460a79c698c5c1229c5651641ee7764c20
Dstreet <doodstreet@gmail.com>
2020-11-13 19:00:23 -0800

    replace ternary with if else
    Corrected dimensions formula var and used if else for more defined code processes.

--- BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -11,23 +11,39 @@ namespace BranchingAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express.Please follow the instructions below.\n");
-            Console.WriteLine("Enter your package weight (to the closest pound):");
+            Console.WriteLine("Enter your package weight:");
             int packLbs = Convert.ToInt32(Console.ReadLine());
-            string checkLbs = packLbs >= 50 ? "Package too heavy to be shipped via Package Express. Have a good day." :
-                                              "\nEnter package width in inches:";
-            Console.WriteLine(checkLbs);
-            int packWidth = Convert.ToInt32(Console.ReadLine());
+            //string checkLbs = packLbs >= 50 ? "Package too heavy to be shipped via Package Express. Have a good day." :
+            //                                  "\nEnter package width in inches:";
+            if (packLbs >= 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("\nEnter package width in inches :");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nEnter package height in inches :");
-            int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nEnter package height in inches :");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nEnter package length in inches :");
-            int packLength = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nEnter package length in inches :");
+                int packLength = Convert.ToInt32(Console.ReadLine());
 
-            decimal shipPrice = ((packWidth * packLength * packHeight * packLbs) / 100);
-            string checkSize = (packWidth + packWidth + packHeight) >= 50 ? "\nPackage too big to ship via Package Express." :
-                                                                            "\nYour estimate for shipping this package is: $" + shipPrice;
-            Console.WriteLine(checkSize);
+                if (packWidth + packLength + packHeight >= 50)
+                {
+                    Console.WriteLine("\nPackage too big to ship via Package Express.");
+                }
+                else
+                {
+                    decimal shipPrice = (packWidth * packLength * packHeight);
+                    decimal totalPrice = (shipPrice * packLbs) / 100;
+
+
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + totalPrice);
+                }
+            }
+            //Console.WriteLine(totalPrice);
 
 
             Console.WriteLine("Thank you");
