9b8e12989127940a3df8b1b34a74e9ea2244e947
Dstreet <doodstreet@gmail.com>
2020-11-16 20:54:08 -0800

    created ClassObjectAssignment

---
+++ ClassObjectAssignment/ClassObjectAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ClassObjectAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number to do some math operations on. ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            mathOp mathClass = new mathOp(); // create an object of type mathOp (class)
+            
+            //mathClass.Multiply(userNum); // get the number from user input
+
+            int result = mathClass.Multiply(userNum);
+            int result1 = mathClass.Divide(userNum);
+            int result2 = mathClass.Add(userNum);
+            Console.WriteLine("Your number times four is " + result);
+            Console.WriteLine("Your number divided by two is " + result1);
+            Console.WriteLine("Your number plus ten is " + result2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassObjectAssignment/ClassObjectAssignment/mathOp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectAssignment
+{
+    public class mathOp // class definition
+    {
+        // class members
+ 
+        // method (or function)
+        public int Multiply(int num)
+        {
+            return num = num * 4;
+        }
+  
+        // second method
+        public int Divide(int num)
+        {
+            return num = num / 2;
+        }
+  
+        public int Add(int num)
+        {
+            return num = num + 10;
+        }
+
+        //// instance constructor
+        //public mathOp()
+        //{
+        //    Number = 0;
+        //}
+
+    }
+}
