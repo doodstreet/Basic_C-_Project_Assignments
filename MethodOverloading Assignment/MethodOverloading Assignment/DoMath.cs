4dd64a597678ea251cab52aabd53046efcf65521
Dstreet <doodstreet@gmail.com>
2020-11-17 17:29:30 -0800

    create MethodOverloadAssignment

---
+++ MethodOverloading Assignment/MethodOverloading Assignment/DoMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace MethodOverloading_Assignment
+{
+    class DoMath
+    {
+        public int Math(int num)
+        {
+            return num * 11;
+        }
+        public decimal Math(decimal num )
+        {
+            return num + 3.14m;
+        }
+        public int Math(string num1)
+        {
+            int x = Convert.ToInt32(num1);
+  
+            return x / 10;
+
+
+
+        }
+         
+    }
+    
+}
---
+++ MethodOverloading Assignment/MethodOverloading Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace MethodOverloading_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoMath math = new DoMath();
+
+            int num1 = math.Math(13);
+            decimal num2 = math.Math(4.60m);
+            int num3 = math.Math("1234");
+            
+            Console.WriteLine("Int: 13 x 11 = " + num1);
+            Console.WriteLine("Decimal: 4.60 + 3.14 = " + num2);
+            Console.WriteLine("String to Int: \"1234\" / 10 = " + num3);
+
+            Console.ReadLine();
+        }
+
+    }
+}
