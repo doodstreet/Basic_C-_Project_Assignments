7434da7956252e5e89342080136eb52e71c9e684
Dstreet <doodstreet@gmail.com>
2020-11-17 22:11:57 -0800

    Void Static Ovreload Assignment

---
+++ Void2Assignment/Void2Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void2Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vmath math = new Vmath();
+
+            Console.WriteLine("Enter a number please");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            math.DoStuff(userInput);
+            int results = math.Math(20, 30);
+            decimal results1 = math.Math(20.5m, 30.6m);
+            int results2 = Vmath.DoMath(13, 11);
+
+            Console.WriteLine("20 + 30 = " + results);
+            Console.WriteLine("20.5 + 30.6 = " + results1);
+            Console.WriteLine("13 * 11 = " + results2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void2Assignment/Void2Assignment/Vmath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void2Assignment
+{
+    public class Vmath
+    {
+        public void DoStuff(int num)
+        {
+            int x = num / 2;
+            Console.WriteLine("Your number divided by two = " + x);
+            return;
+        }
+        public int Math(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public decimal Math(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static int DoMath( int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
