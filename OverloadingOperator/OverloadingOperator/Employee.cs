02bb4f8642be657e51d137286faf7ce46a9001ac
Dstreet <doodstreet@gmail.com>
2020-11-19 23:34:58 -0800

    OverloadOperatorassignment

---
+++ OverloadingOperator/OverloadingOperator/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperator
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Employee emp2 = obj as Employee;
+            if (object.ReferenceEquals(null, emp2)) return false;
+            return Id == emp2.Id;
+        }
+
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            if (object.ReferenceEquals(null, emp1))
+                return object.ReferenceEquals(null, emp2);
+            else if (object.ReferenceEquals(null, emp2))
+                return false;
+            return emp1.Equals(emp2);
+        }
+
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            if (object.ReferenceEquals(null, emp1))
+                return !object.ReferenceEquals(null, emp2);
+            else if (object.ReferenceEquals(null, emp2))
+                return true;
+            return !emp1.Equals(emp2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+    }
+}
---
+++ OverloadingOperator/OverloadingOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { Id = 1, Name = "Dave" };
+            Employee emp2 = new Employee { Id = 2, Name = "Gumby" };
+            bool equalEmployee = emp1 == emp2;
+
+            Employee emp3 = new Employee { Id = 1, Name = "Dave" };
+            equalEmployee = emp1 == emp3;
+            Console.WriteLine(equalEmployee);
+       
+
+            Console.ReadLine();
+        }
+    }
+}
