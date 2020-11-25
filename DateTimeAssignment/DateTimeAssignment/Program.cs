cc1f647c13eb40465f5f27d1c13ff5211443fc29
Dstreet <doodstreet@gmail.com>
2020-11-24 16:00:48 -0800

    DateTimeAssignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("I am asking user for number of choice.");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            DateTime currentTime = DateTime.Now;
+            DateTime xhoursLater = currentTime.AddHours(inputNum);
+            Console.WriteLine(xhoursLater);
+            Console.ReadLine();
+        }
+    }
+}
