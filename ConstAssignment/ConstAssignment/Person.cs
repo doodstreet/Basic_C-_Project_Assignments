9c7dc87be882cec461d969c5b9ed77a9e1fa18f1
Dstreet <doodstreet@gmail.com>
2020-11-24 23:22:35 -0800

    constAssignmnet

---
+++ ConstAssignment/ConstAssignment/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstAssignment
+{
+    public class Person
+    { 
+        public Person (string name) : this(name, 53)
+        {
+        }
+        public Person(string name, int age)
+        {
+            PersonAge = age;
+            Name = name;
+        }
+        public int PersonAge { get; set; }
+        public string Name { get; set; }
+
+        public void NameAge()
+        {
+            Console.WriteLine(Name + " " + PersonAge);
+            return;
+        }
+    }
+}
---
+++ ConstAssignment/ConstAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string title = "Constructors and const";
+            Console.WriteLine("the name of this asssignment is {0}", title);
+
+            var newPerson = new Person("Dave");
+ 
+            newPerson.NameAge();
+            Console.ReadLine();
+        }
+    }
+}
