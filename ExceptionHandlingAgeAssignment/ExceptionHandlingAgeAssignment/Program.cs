a4dc8ac5df5c8ad4b46c853a087b760358574374
Dstreet <doodstreet@gmail.com>
2020-11-28 15:12:43 -0800

    exceptionHandlingAssignment

---
+++ ExceptionHandlingAgeAssignment/ExceptionHandlingAgeAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAgeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+            int now = (DateTime.Today.Year);
+            int birthYear;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("What's your age?");
+                    userAge = Convert.ToInt32(Console.ReadLine());
+                    if (userAge <= 0)
+                        Console.WriteLine("Zero or negative numbers are not valid ages.");
+                } while (userAge <= 0);
+                birthYear = (now - userAge);
+                Console.WriteLine(birthYear + " " +  "Is the year you were born");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
